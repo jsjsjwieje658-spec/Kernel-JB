50edce80a850bcfc950e8c6aeb80098eb21a049b
jsjsjwieje658-spec <jsjsjwieje658@gmail.com>
2026-08-30 17:35:05 +0300

    fix: rename jbctl binary output to uptime_helper (ENOENT on device)
    Previous rename only changed source references but not the Makefile TARGET.
    Binary built as 'jbctl' but code spawned 'uptime_helper' → posix_spawn ENOENT.
    
    Fix: TARGET = jbctl → TARGET = uptime_helper in BaseBin/jbctl/Makefile
         cp jbctl/jbctl → cp jbctl/uptime_helper in BaseBin/Makefile

--- BaseBin/systemhook/upload.sh
+++ BaseBin/systemhook/upload.sh
@@ -8,4 +8,4 @@ make
 # RootHide: Use jbroot path instead of /var/jb
 ssh $DEVICE -p $PORT "rm -rf \$(jbroot)/basebin/$PROJECT_NAME"
 scp -P$PORT ./$PROJECT_NAME $DEVICE:\$(jbroot)/basebin/$PROJECT_NAME
-ssh $DEVICE -p $PORT "\$(jbroot)/basebin/jbctl rebuild_trustcache"
+ssh $DEVICE -p $PORT "\$(jbroot)/basebin/uptime_helper rebuild_trustcache"
